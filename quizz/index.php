68140f337f5724298921cb688b5da676f34ceefa
Paul-Philippe Nelles <ppnelles@gmail.com>
2015-02-03 20:40:13 +0100

    analytics du cul

--- quizz/index.php
+++ quizz/index.php
@@ -88,6 +88,15 @@ include("db.php");
 
 		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 		<script src="../js/quizz.js"></script>
+
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-46068757-1');ga('send','pageview');
+        </script>
 		  
     </body>
 </html>
