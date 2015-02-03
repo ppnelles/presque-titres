f8f8a632a255bf228b5ca2f22df00042d34d4dc4
justine <jle@as-e.be>
2015-02-03 14:52:23 +0100

    Merge with #3 pull request from pixelslip

--- js/main.js
+++ js/main.js
@@ -1,66 +1,64 @@
 var generate = document.getElementById("generate");
 var title = document.getElementById("title");
+var twitter = document.getElementById("twitterbtn");
 var randomTitle = "";
-var twitter = document.getElementById("twitter");
 
-//faits divers initialement créé
-var dico_ville = ["Liège", "Amblève", "Marche", "Vielsalm", "Malmedy", "Gerpine", "Charleroi", "Mons", "Verviers", "Amay", "Ostende", 'Namur', "Schaarbeek", "Bruges", "Namur","Charleroi","Groenland","Texas","Pont-à-Celles","Verviers","Molenbeek","Ixelles","Schaerbeek","Saint-Josse","Anvers"];
-var dico_sujet = ["supporter du standard","supporter d'Anderlecht","unijambiste","nain","chien","Rom","SDF","pédophile","djihadiste","syndicaliste"];
-var dico_verbe = ["tue","tabasse","vend sur ebay","prostitue","décapite","noie","lapide"];
-var dico_qui = ["un supporter d'Anderlecht","un unijambiste","un nain","un chien","un Rom","un SDF","un djihadiste","un enfant","une portée de 19 chats","son lézard de compagnie","son mari alcoolique","un homme tronc"];
-var dico_quoi = ["qui avait mangé ses chips","qui avait bu sa bière", "qui l'avait bousculé dans la rue", "qui l'avait regardé de travers", "qui n'avait rien demandé du tout", "qui attendait le bus", "qui avait raté son train", "à cause de sa femme","qui braquait une banque","parce qu'il est au chômage","pour avoir droit au CPAS","pour entrer au Parti Populaire"];
-
-//second faits divers
-var dico_sujet2 = ["il", "un employé", "Jacques", "Julien", "un papy", "François", "le garagiste"];
-var dico_objet = ["Aidé d'un crucifix","Avec une arbalette","Armé d'une matraque","Muni d'un pied de biche","A l'aide d'une chaise","Avec le nouvel iPhone","Muni d'une clé usb","Avec un cahier Atoma"];
-
-//actu people
-var dico_accroche = ["Incroyable !", "Scoop:", "Inimaginable !", "Exclu SudPresque:", "Fou !", "WOW !!", "Actu people:"];
-var dico_people = ["Barack Obama", "Charles Michel", "Justin Bieber", "Elio Di Rupo", "Bart de Wever", "Maggie de Block", "Jan Jambon", "Raoul Reyers", "Kate Middleton", "Justine Henin", "Nabila", "Michaël Schumacher", "Marc Wilmots", "Steven Defour", "Britney Spears", "Cyril Hanouna", "Kim Kardashian", "Johny Hallyday", "François Hollande", "Sarko", "l'héroïne de Hunger Games"];
-var dico_people_action = ["s'expose une fois de plus sur Instagram", "dans une sextape choc", "très sexy dans une campagne de pub", "fait son coming out", "publie des selfies coquins", "n'écoute jamais La Première", "se prostitue pour payer sa voiture", "plaque tout par amour pour un adolescent !", "se dévoile dans un penthouse londonien"];
-
-//titres à suspense
-var dico_imperatif  = ["Découvrez ce que", "Regardez ce que", "Vous ne devinerez jamais ce que", "C'est époustouflant ce que", "Vous n'imaginez pas ce que"];
-var dico_sujet3     = ["cet homme", "cette femme", "ce chien", "cette petite fille", "ce bébé", "cet énorme chat", "ce vieux croulant", "cette mamy"];
-var dico_action     = ["s'apprête à faire", "est sur le point de faire", "a réalisé", "a cultivé", "a osé faire", "a voulu faire", "a réussi à accomplir"];
-var dico_complement = ["avec une gousse d'ail", "à l'aide d'un simple bout de bois", "avec le chat du voisin", "avec cette botte de poireaux", "avec son arrière-train", "avec une simple patate"];
+// Dictionnaire format obj
+// Note: ça pourrait être cool de pouvoir importer/exporter de nouveaux dictionnaires de cette manière
+var dico = {
+"ville" : ["Liège", "Amblève", "Marche", "Vielsalm", "Malmedy", "Gerpine", "Charleroi", "Mons", "Verviers", "Amay", "Ostende", 'Namur', "Schaarbeek", "Bruges", "Namur","Charleroi","Groenland","Texas","Pont-à-Celles","Verviers","Molenbeek","Ixelles","Schaerbeek","Saint-Josse","Anvers"],
+"sujet" : ["supporter du standard","supporter d'Anderlecht","unijambiste","nain","chien","Rom","SDF","pédophile","djihadiste","syndicaliste"],
+"verbe" : ["tue","tabasse","vend sur ebay","prostitue","décapite","noie","lapide"],
+"qui" : ["un supporter d'Anderlecht","un unijambiste","un nain","un chien","un Rom","un SDF","un djihadiste","un enfant","une portée de 19 chats","son lézard de compagnie","son mari alcoolique","un homme tronc"],
+"quoi" : ["qui avait mangé ses chips","qui avait bu sa bière", "qui l'avait bousculé dans la rue", "qui l'avait regardé de travers", "qui n'avait rien demandé du tout", "qui attendait le bus", "qui avait raté son train", "à cause de sa femme","qui braquait une banque","parce qu'il est au chômage","pour avoir droit au CPAS","pour entrer au Parti Populaire"],
+"sujet2" : ["il", "un employé", "Jacques", "Julien", "un papy", "François", "le garagiste"],
+"objet" : ["Aidé d'un crucifix","Avec une arbalette","Armé d'une matraque","Muni d'un pied de biche","A l'aide d'une chaise","Avec le nouvel iPhone","Muni d'une clé usb","Avec un cahier Atoma"],
+"accroche" : ["Incroyable !", "Scoop:", "Inimaginable !", "Exclu SudPresque:", "Fou !", "WOW !!", "Actu people:"],
+"people" : ["Barack Obama", "Charles Michel", "Justin Bieber", "Elio Di Rupo", "Bart de Wever", "Maggie de Block", "Jan Jambon", "Raoul Reyers", "Kate Middleton", "Justine Henin", "Nabila", "Michaël Schumacher", "Marc Wilmots", "Steven Defour", "Britney Spears", "Cyril Hanouna", "Kim Kardashian", "Johny Hallyday", "François Hollande", "Sarko", "l'héroïne de Hunger Games"],
+"people_action" : ["s'expose une fois de plus sur Instagram", "dans une sextape choc", "très sexy dans une campagne de pub", "fait son coming out", "publie des selfies coquins", "n'écoute jamais La Première", "se prostitue pour payer sa voiture", "plaque tout par amour pour un adolescent !", "se dévoile dans un penthouse londonien"],
+"imperatif" : ["Découvrez ce que", "Regardez ce que", "Vous ne devinerez jamais ce que", "C'est époustouflant ce que", "Vous n'imaginez pas ce que"],
+"sujet3" : ["cet homme", "cette femme", "ce chien", "cette petite fille", "ce bébé", "cet énorme chat", "ce vieux croulant", "cette mamy"],
+"action" : ["s'apprête à faire", "est sur le point de faire", "a réalisé", "a cultivé", "a osé faire", "a voulu faire", "a réussi à accomplir"],
+"complement" : ["avec une gousse d'ail", "à l'aide d'un simple bout de bois", "avec le chat du voisin", "avec cette botte de poireaux", "avec son arrière-train", "avec une simple patate"]
+};
 
 function titleRandom() {
+  var ville, sujet, sujet2, verbe, objet, qui, quoi, accroche, people, people_action, imperatif, sujet3, action, complement;
+  var titleNumber = parseInt((Math.random() * 4) + 1, 10);
 
-  var ville         = dico_ville[Math.floor(Math.random() * dico_ville.length)];
-  var sujet         = dico_sujet[Math.floor(Math.random() * dico_sujet.length)];
-  var sujet2        = dico_sujet2[Math.floor(Math.random() * dico_sujet2.length)];
-  var verbe         = dico_verbe[Math.floor(Math.random() * dico_verbe.length)];
-  var objet         = dico_objet[Math.floor(Math.random() * dico_objet.length)];
-  var qui           = dico_qui[Math.floor(Math.random() * dico_qui.length)];
-  var quoi          = dico_quoi[Math.floor(Math.random() * dico_quoi.length)];
-  var accroche      = dico_accroche[Math.floor(Math.random() * dico_accroche.length)];
-  var people        = dico_people[Math.floor(Math.random() * dico_people.length)];
-  var people_action = dico_people_action[Math.floor(Math.random() * dico_people_action.length)];
-  var imperatif     = dico_imperatif[Math.floor(Math.random() * dico_imperatif.length)];
-  var sujet3        = dico_sujet3[Math.floor(Math.random() * dico_sujet3.length)];
-  var action        = dico_action[Math.floor(Math.random() * dico_action.length)];
-  var complement    = dico_complement[Math.floor(Math.random() * dico_complement.length)];
-
-  var titleNumber = Math.floor((Math.random() * 4) + 1);
-
-  if (titleNumber == 1) {
-    randomTitle = ville+': un '+sujet+' '+verbe+' '+qui+' '+quoi;
+  switch (titleNumber) {
+    case 1:
+      ville = dico.ville[parseInt(Math.random() * dico.ville.length, 10)];
+      sujet = dico.sujet[parseInt(Math.random() * dico.sujet.length, 10)];
+      verbe = dico.verbe[parseInt(Math.random() * dico.verbe.length, 10)];
+      qui = dico.qui[parseInt(Math.random() * dico.qui.length, 10)];
+      quoi = dico.quoi[parseInt(Math.random() * dico.quoi.length, 10)];
+      randomTitle = ville+': un '+sujet+' '+verbe+' '+qui+' '+quoi;
+      break;
+    case 2:
+      sujet2 = dico.sujet2[parseInt(Math.random() * dico.sujet2.length, 10)];
+      verbe = dico.verbe[parseInt(Math.random() * dico.verbe.length, 10)];
+      objet = dico.objet[parseInt(Math.random() * dico.objet.length, 10)];
+      qui = dico.qui[parseInt(Math.random() * dico.qui.length, 10)];
+      quoi = dico.quoi[parseInt(Math.random() * dico.quoi.length, 10)];
+      randomTitle = objet+', '+sujet2+' '+verbe+' '+qui+' '+quoi;
+      break;
+    case 3:
+      imperatif = dico.imperatif[parseInt(Math.random() * dico.imperatif.length, 10)];
+      sujet3 = dico.sujet3[parseInt(Math.random() * dico.sujet3.length, 10)];
+      action = dico.action[parseInt(Math.random() * dico.action.length, 10)];
+      complement = dico.complement[parseInt(Math.random() * dico.complement.length, 10)];
+      randomTitle = imperatif+' '+sujet3+' '+action+' '+complement;
+      break;
+    default:
+      accroche = dico.accroche[parseInt(Math.random() * dico.accroche.length, 10)];
+      people = dico.people[parseInt(Math.random() * dico.people.length, 10)];
+      people_action = dico.people_action[parseInt(Math.random() * dico.people_action.length, 10)];
+      randomTitle = accroche+' '+people+' '+people_action;
   }
-  else if (titleNumber == 2) {
-    randomTitle = objet+', '+sujet2+' '+verbe+' '+qui+' '+quoi;
-  }
-  else if (titleNumber == 3) {
-    randomTitle = accroche+' '+people+' '+people_action;
-  }
-  else if (titleNumber == 4) {
-    randomTitle = imperatif+' '+sujet3+' '+action+' '+complement;
-  }
-
 
   title.innerHTML = randomTitle;
   twitter.href="https://twitter.com/intent/tweet?text="+randomTitle+" -&url=http://presquetitres.be";
-
 }
 
 window.onload = titleRandom;
